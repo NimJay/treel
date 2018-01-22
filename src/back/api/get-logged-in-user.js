d79204339c8a768d43f768d5fbf0b659364db22f
Nim <i.am.nim.jay@gmail.com>
2018-01-22 18:32:59 -0500

    Support /api/get-logged-in-user to retrieve logged in User

--- src/back/api/get-logged-in-user.js
+++ src/back/api/get-logged-in-user.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose');
+const Output = require('./util/Output.js').Output;
+
+/**
+ * Retrieves the User currently logged in.
+ * Example output: { user: {...} }
+ * If not logged in, The user will be null.
+ */
 function post(req, res) {
-    res.json('TODO: Implement /get-logged-in-user.');
+    var o = new Output(res),
+        userId = req.session.userId;
+    if (!userId) return o.set('user', null).out();
+    var User = mongoose.model('User');
+    User.findById(userId, function (err, user) {
+        if (err) return o.err('DATABASE').out();
+        return o.set('user', user).out();
+    });
 }
 
 module.exports = { post };
