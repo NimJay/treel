29c047b0770d6a931e536264a9e95c74bf81e133
Nim <i.am.nim.jay@gmail.com>
2018-01-21 21:07:11 -0500

    Reorder middleware to active sessions

--- src/back/Server.js
+++ src/back/Server.js
@@ -7,28 +7,31 @@ const api = require('./api/ApiRouter.js');
 const PORT = require('./Config').PORT;
 const { connect } = require('./Mongo.js');
 
-app.use(express.static(__dirname + '/../public')); // Serve this directory.
-app.use('/api', api); // Treel API.
-
-// Send index.html for all pages.
-var sendIndex = (req, res) => {res.sendFile(path.resolve(__dirname + '/../public/index.html'));}
-app.get('/about', sendIndex);
-app.get('/sign-up', sendIndex);
-
-// Everything else: index.html with 404.
-app.use((req, res) => {
-    res.status(404);
-    res.sendFile(__dirname + '/../public/index.html');
-});
-
 // Connect to Mongo, set up sessions, and run server.
 connect(function (db) {
+
+    // Setup session.
     app.use(session({
         secret: 'TreelSessionSecret',
         resave: false,
         saveUninitialized: true,
         store: new MongoStore({ 'mongooseConnection': db })
     }));
+
+    app.use(express.static(__dirname + '/../public')); // Serve this directory.
+    app.use('/api', api); // Treel API.
+
+    // Send index.html for all pages.
+    var sendIndex = (req, res) => {res.sendFile(path.resolve(__dirname + '/../public/index.html'));}
+    app.get('/about', sendIndex);
+    app.get('/sign-up', sendIndex);
+
+    // Everything else: index.html with 404.
+    app.use((req, res) => {
+        res.status(404);
+        res.sendFile(__dirname + '/../public/index.html');
+    });
+
     app.listen(PORT, function () {
         console.log('Serving Treel at: http://localhost:' + PORT);
     });
