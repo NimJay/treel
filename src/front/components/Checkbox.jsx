1657ee679bb6eceae046d32de27bea92e4e73d02
Nim <i.am.nim.jay@gmail.com>
2018-02-21 17:00:36 -0500

    Introduce <Checkbox> to close #25

---
+++ src/front/components/Checkbox.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+/**
+ * Checkbox
+ * className: className for the outer most <label>.
+ * label: The label.
+ * style: style for the outer most <label>.
+ * value: true of false.
+ * onChange(value): function to call when (un)checked.
+ */
+class Checkbox extends React.Component {
+
+    onChange(e) {
+        console.log(4);
+        let { value, onChange } = this.props;
+        if (onChange) onChange(!value);
+    }
+
+    render() {
+        let { style, className, label, value } = this.props;
+
+        // className
+        className = className ? className : '';
+
+        return (
+            <label className={"checkbox " + (value ? "checked " : "")
+                + className} style={style}>
+                <span>
+                    {value && <span className="icon icon-check"></span>}
+                </span>
+                <span>{label}</span>
+                <input type="checkbox" checked={value}
+                    onChange={this.onChange.bind(this)} />
+            </label>
+        );
+    }
+}
+
+
+export default Checkbox;
