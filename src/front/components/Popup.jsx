f526db4ed8ac4598bec0cdcd35ba4d4aabe161b4
Nim <i.am.nim.jay@gmail.com>
2018-02-22 13:12:59 -0500

    Remove redundant prop in <Popup>

--- src/front/components/Popup.jsx
+++ src/front/components/Popup.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Popup = ({ children, isClosable, onClose }) => (
+const Popup = ({ children, onClose }) => (
     <div className="popup-wall">
         <div className="popup">
             <div className="popup-close">
-                {!isClosable &&
+                {onClose &&
                     <span onClick={onClose} className="icon icon-close"></span>}
             </div>
             {children}
