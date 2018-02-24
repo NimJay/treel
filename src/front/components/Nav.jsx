4c493b6221e186af86b1bb9b8f1d7cf8c4bb7b13
Nim <i.am.nim.jay@gmail.com>
2018-02-24 16:40:21 -0500

    Make <Nav> toggle onMouseEnter

--- src/front/components/Nav.jsx
+++ src/front/components/Nav.jsx
@@ -2,16 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton.jsx';
 
-const Nav = ({ app, setApp }) => {
-    return (
-        <nav className="navigation block">
-            <div className="row">
-                <Link to="/">Home</Link>
-                {app.isLoggedIn() &&
-                    <LogoutButton setApp={setApp} isMini={true} />}
-            </div>
-        </nav>
-    );
+class Nav extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showLinks: false
+        }
+    }
+
+    showLinks() {this.setState({ showLinks: true });}
+    hideLinks() {this.setState({ showLinks: false });}
+
+    render () {
+        let { app, setApp } = this.props,
+            { showLinks } = this.state;
+
+        if (!showLinks) {
+            return (
+                <nav className="navigation block row"
+                    onMouseEnter={this.showLinks.bind(this)}>
+                    <span className="icon icon-menu"></span>
+                </nav>
+            );
+        }
+
+        return (
+            <nav className="navigation block"
+                onMouseLeave={this.hideLinks.bind(this)}>
+                <div className="row">
+                    <span className="icon icon-menu"></span>
+                    <Link to="/">Home</Link>
+                    {app.isLoggedIn() &&
+                        <LogoutButton setApp={setApp} isMini={true} />}
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default Nav;
