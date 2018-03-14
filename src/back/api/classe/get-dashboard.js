e9572ad45e8c56709ce9b220f3daa22abdd6a3a8
Nim <i.am.nim.jay@gmail.com>
2018-03-13 21:33:11 -0400

    Output Classes Followed in get-dashboard.js to close #18

--- src/back/api/classe/get-dashboard.js
+++ src/back/api/classe/get-dashboard.js
@@ -5,28 +5,39 @@ const getLoggedInUserId = require('../util/Global.js').getLoggedInUserId;
 
 function post(req, res) {
 
-    var Classe = mongoose.model('Classe');
-    var School = mongoose.model('School');
-    var o = new Output(res);
+    var Classe = mongoose.model('Classe'),
+        Follow = mongoose.model('Follow'),
+        School = mongoose.model('School'),
+        o = new Output(res);
 
     var userId = getLoggedInUserId(req);
     if (!userId) return o.err('NOT_LOGGED_IN').out();
 
-    // Find the Classes created/instructed by User.
-    Classe.find(
-        { $or: [ { creator: userId }, { instructors: userId } ] },
-        function (err, classes) {
-            if (err) return o.err('DATABASE').out();
-            o.set('classes', classes);
+    // Retrieve User's Follows.
+    Follow.find({ user: userId }, (err, fs) => {
+        if (err || !fs) return o.err('NOT_LOGGED_IN').out();
+        var followIds = fs.map(f => f.classe);
 
-            // Gather relevant Schools.
-            var sIds = classes.map(c => c.school);
-            School.find({ _id: { $in: sIds } }, function (err, schools) {
+        // Find the Classes created/instructed/followed by User.
+        Classe.find(
+            { $or: [
+                { creator: userId },
+                { instructors: userId },
+                { _id: { $in: followIds } }
+            ] },
+            function (err, classes) {
                 if (err) return o.err('DATABASE').out();
-                return o.set('schools', schools).out();
-            });
-        }
-    );
+                o.set('classes', classes);
+
+                // Gather relevant Schools.
+                var sIds = classes.map(c => c.school);
+                School.find({ _id: { $in: sIds } }, function (err, schools) {
+                    if (err) return o.err('DATABASE').out();
+                    return o.set('schools', schools).out();
+                });
+            }
+        );
+    });
 }
 
 
