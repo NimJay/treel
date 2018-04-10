7104300381071147f62e5ed43c39a194ed35b73c
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2018-04-10 14:19:05 -0400

    Hide edit icons to close #74

--- src/front/sections/SectionSection/Content/ContentDiv.jsx
+++ src/front/sections/SectionSection/Content/ContentDiv.jsx
@@ -10,44 +10,68 @@ import ContentEditor from './ContentEditor.jsx';
  *    classe, section, content, isEditable, isFirst, isLast, onMove(isMoveUp),
  *    onDeletion(), onUpdate(content)
  */
-const ContentDiv = ({ classe, section, content, isEditable, isFirst, isLast,
-    onMove, onDeletion, onUpdate }) => {
+class ContentDiv extends React.Component {
 
-    let div = null;
-
-    if (content.type == 'text') {
-        div = (<p>{content.text.split('\n')
-            .map((a, i) => <span key={i}>{a}<br/></span>)}</p>);
+    constructor(props) {
+        super(props);
+        this.state = {
+            showIcons: false
+        }
     }
 
-    else if (content.type == 'link') {
-        let link = content.link;
-        if (!link.match(/^https?:\/\//i)) link = 'http://' + link;
-        div = (<p><a href={link}>{content.name}</a> {content.description}</p>);
+    showIcons() {this.setState({ showIcons: true });}
+    hideIcons() {this.setState({ showIcons: false });}
 
-    } else if (content.type == 'file') {
-        div = (<p><a href={"/file/" + content.file}>
-            {content.name}</a> {content.description}</p>);
+    render() {
+        let { classe, section, content, isEditable, isFirst, isLast,
+            onMove, onDeletion, onUpdate } = this.props,
+            { showIcons } = this.state;
+
+        let div = null;
+        if (content.type == 'text') {
+            div = (<p>{content.text.split('\n')
+                .map((a, i) => <span key={i}>{a}<br/></span>)}</p>);
+
+        } else if (content.type == 'link') {
+            let link = content.link;
+            if (!link.match(/^https?:\/\//i)) link = 'http://' + link;
+            div = (<p><a href={link}>{content.name}</a> {content.description}</p>);
+
+        } else if (content.type == 'file') {
+            div = (<p><a href={"/file/" + content.file}>
+                {content.name}</a> {content.description}</p>);
+        }
+
+
+        return (
+            <div className={"contentdiv row" + (isEditable ? ' editable' : '')}>
+
+                {isEditable && showIcons &&
+                    <div className='contentdiv-icons'
+                        onMouseLeave={this.hideIcons.bind(this)}>
+                        <ContentDeleter classeId={classe._id} sectionId={section._id}
+                            content={content} onDeletion={onDeletion} />
+                        <ContentMover classeId={classe._id} sectionId={section._id}
+                            contentId={content._id} isMoveUp={false}
+                            disabled={isLast} onMove={null} onMove={onMove} />
+                        <ContentMover classeId={classe._id} sectionId={section._id}
+                            contentId={content._id} isMoveUp={true}
+                            disabled={isFirst} onMove={null} onMove={onMove} />
+                        <ContentEditor classeId={classe._id} sectionId={section._id}
+                            content={content} onUpdate={onUpdate} />
+                        <span className='icon icon-more-horizontal'></span>
+                    </div>}
+
+                {isEditable && !showIcons &&
+                    <div className='contentdiv-icons'>
+                        <span className='icon icon-more-horizontal'
+                            onMouseEnter={this.showIcons.bind(this)}></span>
+                    </div>}
+
+                {div}
+            </div>
+        );
     }
-
-    return (
-        <div className={"contentdiv row" + (isEditable ? ' editable' : '')}>
-            {isEditable &&
-                <div className="contentdiv-icons">
-                    <ContentDeleter classeId={classe._id} sectionId={section._id}
-                        content={content} onDeletion={onDeletion} />
-                    <ContentMover classeId={classe._id} sectionId={section._id}
-                        contentId={content._id} isMoveUp={false}
-                        disabled={isLast} onMove={null} onMove={onMove} />
-                    <ContentMover classeId={classe._id} sectionId={section._id}
-                        contentId={content._id} isMoveUp={true}
-                        disabled={isFirst} onMove={null} onMove={onMove} />
-                    <ContentEditor classeId={classe._id} sectionId={section._id}
-                        content={content} onUpdate={onUpdate} />
-                </div>}
-            {div}
-        </div>
-    );
 }
 
 
