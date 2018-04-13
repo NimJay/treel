2cc4ca51c764b3c52a150019feb8fedb5724a307
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2018-04-13 15:03:39 -0400

    Compose Verification Email to close #84

--- src/back/Config.js
+++ src/back/Config.js
@@ -2,6 +2,7 @@ const CONFIG = {
     'PORT': 3003,
     'MONGO_URI': 'mongodb://localhost:27017/treel',
     'FILES_DIR': __dirname + '/file/',
+    'URL': 'http://localhost:3003',
 
     // Treel requires an SMPT email account to deliver all emails.
     'EMAIL': 'treel.app@gmail.com',
--- src/back/api/util/Emailer.js
+++ src/back/api/util/Emailer.js
@@ -12,6 +12,30 @@ let transporter = nodemailer.createTransport({
 });
 
 
+function sendVerification(email, code, callback) {
+
+    let link = `${CONFIG.URL}/verify/${code}`,
+		html = `
+<div style="font-size: 20px;font-family: sans-serif;text-align: center;padding: 50px 10px;">
+    <a style="font-family: sans-serif;display: inline-block;font-size: 26px;background-color: #70dc52;color: #266715;padding: 20px;text-decoration: none;border-radius: 5px;" href="${link}">Verify Email</a>
+    <p style="font-family: sans-serif;font-size: 20px;">Click on the button above to verify your email address.</p>
+    <p style="font-family: sans-serif;font-size: 16px;">Or visit this link: <a href="${link}">${link}</a></p>
+</div>`;
+
+    let options = {
+        'from': `Treel <${CONFIG.EMAIL}>`,
+        'to': email,
+        'subject': 'Treel — Email Verification',
+        'html': html
+    };
+
+    transporter.sendMail(options, callback ? (err, info) => {
+        if (err || !info) callback(err, null, null);
+        callback(err, info.accepted);
+    } : null);
+}
+
+
 function announce(text, user, classe, emails, callback) {
 
     emails = emails
@@ -33,4 +57,4 @@ function announce(text, user, classe, emails, callback) {
 }
 
 
-module.exports = { announce };
+module.exports = { announce, sendVerification };
