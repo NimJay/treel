c485a7d594a8a0e3f2c18c56f10cab522cb42aa6
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2018-04-13 18:28:16 -0400

    Make minor fix to <AboutPage>

--- src/front/pages/AboutPage.jsx
+++ src/front/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => (
             <Link to="/"><img src="/favicon.png" alt="Treel Logo, a 2D graphic of a tree" /></Link>
             <h1>Treel</h1>
             <p>Treel is a minimalistic <em>learning management system</em> that allows instructors to distribute course content to students.</p>
-            <p>It was created as a school project by <a href="http://individual.utoronto.ca/nim" target="_blank">Nimesha Jayawardena</a> via the surpervision of <a href="http://www.cs.utoronto.ca/~ylzhang/">Professor Larry Zhang</a>. The source code can be found on <a href="https://github.com/NimJay/treel" target="_blank">Github</a>.</p>
+            <p>It was created as a school project by <a href="http://individual.utoronto.ca/nim" target="_blank">Nimesha Jayawardena</a> via the surpervision of <a href="http://www.cs.utoronto.ca/~ylzhang/" target="_blank">Professor Larry Zhang</a>. The source code can be found on <a href="https://github.com/NimJay/treel" target="_blank">Github</a>.</p>
             <div className="buttons-left"><Link to="/" className="button">Login</Link> or <Link to="/sign-up">sign up here</Link>.</div>
         </main>
     </div>
