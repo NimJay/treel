2c1113d3c5a40274964a490be9c9b11d47ae411a
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2018-04-15 11:02:54 -0400

    Change port used

--- src/back/Config.js
+++ src/back/Config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    'PORT': 3003,
+    'PORT': 8080,
     'MONGO_URI': 'mongodb://localhost:27017/treel',
     'FILES_DIR': __dirname + '/file/',
-    'URL': 'http://localhost:3003',
+    'URL': 'http://localhost:8080',
 
     // Treel requires an SMPT email account to deliver all emails.
     'EMAIL': 'treel.app@gmail.com',
